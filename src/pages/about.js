3667b938fa7bda9f9930b5c85116ff8b2ec26525
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-09-15 19:13:26 +0100

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -55,19 +55,19 @@ const about = () => {
                 Biography
               </h2>
               <p className="font-medium dark:text-light/75">
-                Hi, I'm Joseph, Graphic designer and UI/UX designer with a
+                Hi, I&apos;m Joseph, Graphic designer and UI/UX designer with a
                 passion for creating beautiful, functional, and user-centered
                 digital experiences. With 5 years of experience in the field. I
                 am always looking for new and innovative ways to bring my
-                clients' visions to life.
+                clients visions to life.
               </p>
               <p className="my-4 font-medium dark:text-light/75">
                 I believe that design is about more than just making things look
-                pretty – it's about solving problems and creating intuitive,
+                pretty. it's about solving problems and creating intuitive,
                 enjoyable experiences for users.
               </p>
               <p className="font-medium dark:text-light/75">
-                Whether I'm working on a website, mobile app, or other digital
+                Whether I&apos;m working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
