1dec2f8cb96a9a744ed45c4f755e72b635b15707
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-11-26 08:11:14 +0100

    update

--- src/pages/projects/social-media/index.js
+++ src/pages/projects/social-media/index.js
@@ -4,16 +4,9 @@ import TransitionEffect from "@/components/TransitionEffect";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import CM from "../../../../public/images/projects/CM.jpg";
-import HF from "../../../../public/images/projects/HF.jpg";
-import letskate from "../../../../public/images/projects/LetsSkate.gif";
-import pf from "../../../../public/images/projects/PF.jpg";
-import venn from "../../../../public/images/projects/Venn.jpg";
 import afa from "../../../../public/images/projects/afa.jpg";
-import blonk from "../../../../public/images/projects/blonk.jpg";
-import estate from "../../../../public/images/projects/estate.jpg";
-import icookpad from "../../../../public/images/projects/iCookPad.gif";
-import rps from "../../../../public/images/projects/rps.gif";
+import gymrat from "../../../../public/images/projects/gymrat.jpg";
+import kiki from "../../../../public/images/projects/kiki.jpg";
 
 import { useState } from 'react'; // Import useState
 
@@ -110,36 +103,7 @@ const Projects = () => {
             text="Imagination Trumps Knowledge!"
             className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
           />
-
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm::gap-y-0">
-            <div className="col-span-12">
-              <FeaturedProject
-                title="Blonk : Executive Search "
-                img={blonk}
-                summary="Blonk is a new way to recruit, combining cutting-edge digital marketing and best headhunting techniques. 
-                I redesigned Blonk's UI/UX, changing the overall structure, Enhancing the UI and Adding animations."
-                link="https://blonk.co"
-                type="Web & Mobile App UI/UX Design"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              {" "}
-              <Project
-                title="Venn Telecom"
-                img={venn}
-                link="https://www.venntelecom.com"
-                type="UI Redesign"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              {" "}
-              <Project
-                title="E-state: Real estate company"
-                img={estate}
-                link="https://www.figma.com/file/anzu6Wb4mE0uDlHVu92HKj/E-state?type=design&node-id=192-2455&mode=design&t=fRyXUy2kSaYa4UJ8-11"
-                type="Web UI/UX Design"
-              />
-            </div>
             <div className="col-span-12">
               {" "}
               <FeaturedProject
@@ -150,59 +114,22 @@ const Projects = () => {
                 type="UI/UX Design"
               />
             </div>
-
-            <div className="col-span-12">
-              {" "}
-              <FeaturedProject
-                title="PROFHIMA"
-                img={pf}
-                summary="PROFHIMA is a construction company based in France, I helped the owner to design his logo, then furniture such as business card, envelope, shirts... Then I designed his website, built it and deployed online, a satisfied client happy with the service I provided."
-                link="https://profhima.fr"
-                type="Branding || Graphic design"
-              />
-            </div>
             <div className="col-span-6 sm:col-span-12">
+              {" "}
               <Project
-                title="High Fit: Workout plans"
-                img={HF}
-                link="https://www.figma.com/file/anzu6Wb4mE0uDlHVu92HKj/E-state?type=design&node-id=192-2455&mode=design&t=fRyXUy2kSaYa4UJ8-11"
-                type="Web UI/UX Design"
+                title="Gymrat"
+                img={gymrat}
+                link="https://www.instagram.com/gymrat.tn/"
+                type="Clothing Design || Social Media Design"
               />
             </div>
             <div className="col-span-6 sm:col-span-12">
               {" "}
               <Project
-                title="Crypto Marketplace"
-                img={CM}
-                link="https://www.figma.com/file/OtTYhZcSQq4R2i1n0Yug4O/Crypto-Market-(work-in-progress)?type=design&mode=design&t=J0lcGQNI8nuWoKLZ-11"
-                type="Mobile App UI/UX Design"
-              />
-            </div>
-            <div className="col-span-12">
-              {" "}
-              <FeaturedProject
-                title="RPS Multiplayer"
-                img={rps}
-                summary="Modern Rock Paper Scissors game design, made using Figma and Illustrator for the game components and illustrations, Designed for iPhone respecting apple's HIG (Human Interface Guidelines)."
-                link="https://www.figma.com/file/Vq6osRrEemkROeWGVcpVhE/RPS-Multiplayer?type=design&mode=design"
-                type="Mobile App UI/UX Design"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="Let's Skate: Social Media"
-                img={letskate}
-                link="https://github.com/JoJoDevAdventure/Let-s-Skate"
-                type="Mobile App UI/UX Design"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              {" "}
-              <Project
-                title="iCookPad"
-                img={icookpad}
-                link="https://github.com/JoJoDevAdventure/iCookPad"
-                type="Mobile App UI/UX Design"
+                title="KiKi: Luxury beach accessories"
+                img={kiki}
+                link="https://www.instagram.com/kikibeach_conceptstore/"
+                type="Branding || Social Media Design"
               />
             </div>
           </div>
